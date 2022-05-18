31f3f9b09ac53559d95f97d94efe9b779a1d5f97
1Gracia0 <gracia_marca@usmp.pe>
2022-05-17 20:29:53 -0500

    Iconos footer

---
+++ Controllers/PagoController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Inkasign.Data;
+using Inkasign.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Dynamic;
+
+namespace Inkasign.Controllers
+{
+    public class PagoController : Controller
+    {
+       
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public PagoController(ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
+        {      
+
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index(Decimal MontoTotal)
+        {
+            Pago pago = new Pago();
+            pago.MontoTotal = MontoTotal;
+            return View(pago);
+        }
+
+        [HttpPost]
+
+        public IActionResult Pagar(Pago pago){
+
+            return null;
+        }
+
+        
+    }
+}
